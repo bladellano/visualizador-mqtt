e5983352d4ec63e9ad260e33dac52e3175b14cc3
Caio <bladellano@gmail.com>
2024-06-07 19:18:46 +0100

    :sparkles:Adicionando middleware de compressão no Laravel.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ApiController;
+use ErlandMuchasaj\LaravelGzip\Middleware\GzipEncodeResponse;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -14,4 +15,6 @@ Route::get('/registers-events', [ApiController::class, 'getRegistersByEvents']);
 Route::get('/registers-details/{id}/{type}', [ApiController::class, 'eventDetails']);
 Route::get('/mensagens-um-valor', [ApiController::class, 'getMessageOneValue']);
 
-Route::get('/get-events', [ApiController::class, 'getEvents']);
+Route::middleware([GzipEncodeResponse::class])->group(function () {
+    Route::get('/get-events', [ApiController::class, 'getEvents']);
+});
